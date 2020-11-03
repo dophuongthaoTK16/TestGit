b99ce8c071193af84cd791ecc947ba057d5dbec7
dophuongthaoTK16 <dophuongthaotk166@gmail.com>
2020-11-03 15:34:16 +0700

    Đẩy code lên GitHub

---
+++ Webbanhang/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webbanhang.Models;
+using System.Data;
+namespace Webbanhang.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        DatHangModel dbdh = new DatHangModel();
+        public ActionResult Index()
+        {
+            List<DatHang> dsdh = dbdh.getalldonhang(); ;
+
+            return View(dsdh);
+        }
+    }
+}
